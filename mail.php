d9a8ca720ab99e5e91aa3f9e6bc7097ec87a3815
ediz12 <ediztekok@gmail.com>
2019-02-04 02:26:52 +0300

    fixed mail

--- mail.php
+++ mail.php
@@ -1,31 +1,34 @@
 <?php
     $to = 'ediztekok@gmail.com';
-    $name = $_POST["name"];
-    $email= $_POST["email"];
-    $subject= $_POST["subject"];
-    $msg = $_POST["message"];
+    $name = strip_tags($_POST["name"]);
+    $email= strip_tags($_POST["email"]);
+    $subject= strip_tags($_POST["subject"]);
+    $msg = strip_tags($_POST["message"]);
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    
+    $message = '<html><body>';
+    $message .= '<table rules="all" style="border-color: #666;" border="3" cellpadding="10">';
+    $message .= '<tr><td colspan="2"><img src="https://media1.tenor.com/images/b4a913503484d9120abbf56845aedac3/tenor.gif?itemid=10880734" alt="You have got a mail!" style="width: 100%; height: fit-content"/></td></tr>';
+    $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . $name . "</td></tr>";
+    $message .= "<tr><td><strong>Email:</strong> </td><td>" . $email . "</td></tr>";
+    $message .= "<tr><td><strong>Subject:</strong> </td><td>" . $subject . "</td></tr>";
+    $message .= "<tr><td><strong>Message:</strong> </td><td>" . $msg . "</td></tr>";
+    $message .= "</table>";
+    $message .= "</body></html>";
+    $message = nl2br($message);
 
-    $message ='<table style="width:100%">
-        <tr>
-            <td>Name: '.$name.'</td>
-        </tr>
-        <tr>
-	    <td>Email: '.$email.'</td>
-        </tr>
-        <tr>
-	    <td>Message: '.$msg.'</td>
-        </tr>
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'The message has been sent.';
-    }else{
-        echo 'failed';
+    if (empty($name) or empty($email) or empty($subject) or empty($msg)){
+            echo "Invalid input!";
     }
-
+    else{
+        if (@mail($to, $subject, $message, $headers))
+            {
+                echo 'Thank you for your message on subject "' .$subject. '"! I will get back to you shortly.';
+            }else{
+                echo 'Sorry, failed to send your message on "' .$subject. '"! Please try again. If this problem persists, please let me know at ediztekok at gmail.com';
+            }   
+    }
 ?>
